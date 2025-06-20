735017b455dbf0ebd1bac70ff8966c87f146374f
Ricky Adriell <53133228+rickyadriell@users.noreply.github.com>
2025-06-20 19:48:16 +0100

    init

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-xl text-center justify-center">
+        <h1 className="text-4xl font-bold tracking-tight text-default-900 dark:text-white 0 sm:text-5xl">
+          Experience AI
+        </h1>
+      </div>
+    </section>
+  );
+}
